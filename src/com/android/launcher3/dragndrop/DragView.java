dd6bb24bb9199f765023ef401e3dd7d418f49f66
Amir Zaidi <azaidi@live.nl>
2019-08-13 17:56:04 +0200

    Launcher3: Keep spring adaptive icon hidden until drag starts

--- src/com/android/launcher3/dragndrop/DragView.java
+++ src/com/android/launcher3/dragndrop/DragView.java
@@ -109,6 +109,7 @@ public class DragView extends View implements LauncherStateManager.StateListener
     private Path mScaledMaskPath;
     private Drawable mBadge;
     private ColorMatrixColorFilter mBaseFilter;
+    private boolean mShowSpringIcon;
 
     /**
      * Construct the drag view.
@@ -397,7 +398,7 @@ public class DragView extends View implements LauncherStateManager.StateListener
             }
         }
 
-        if (mScaledMaskPath != null) {
+        if (mScaledMaskPath != null && mShowSpringIcon) {
             int cnt = canvas.save();
             canvas.clipPath(mScaledMaskPath);
             mBgSpringDrawable.draw(canvas);
@@ -516,6 +517,9 @@ public class DragView extends View implements LauncherStateManager.StateListener
     public void move(int touchX, int touchY) {
         if (touchX > 0 && touchY > 0 && mLastTouchX > 0 && mLastTouchY > 0
                 && mScaledMaskPath != null) {
+            if (mLastTouchX != touchX || mLastTouchY != touchY) {
+                mShowSpringIcon = true;
+            }
             mTranslateX.animateToPos(mLastTouchX - touchX);
             mTranslateY.animateToPos(mLastTouchY - touchY);
         }
